2929da5283217f3525bd693436db23f553525a12
jpgiovanelli <110671520+jpgiovanelli@users.noreply.github.com>
2023-10-10 14:50:11 -0300

    Add files via upload

---
+++ Pratica 6 ap1.ino
@@ -0,0 +1,40 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+
+  pinMode(11,INPUT);
+  pinMode(12,INPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(11) == 0 and digitalRead(12) == 0) {
+    digitalWrite(5, 1);
+    digitalWrite(6, 1);
+    digitalWrite(7, 1);
+  }
+
+  if (digitalRead(11) == 1 and digitalRead(12) == 0) {
+    digitalWrite(5, 1);
+    digitalWrite(6, 0);
+    digitalWrite(7, 0);
+  }
+
+  if (digitalRead(11) == 0 and digitalRead(12) == 1) {
+    digitalWrite(5, 0);
+    digitalWrite(6, 1);
+    digitalWrite(7, 0);
+  }
+
+  if (digitalRead(11) == 1 and digitalRead(12) == 1) {
+    digitalWrite(5, 0);
+    digitalWrite(6, 0);
+    digitalWrite(7, 0);
+  }
+
+}
