d83cbcb056ada6579aa3423ad4bab7702610a714
Ian Esteves Amaral Francisco <99906106+Tredost@users.noreply.github.com>
2023-10-10 21:39:29 -0300

    Update Pratica 6 ap1.ino

--- Pratica 6 ap1.ino
+++ Pratica 6 ap1.ino
@@ -1,34 +1,39 @@
+#define led1 5
+#define led2 6
+#define led3 7
+#define chave1 11
+#define chave2 12
+
 void setup() {
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
-  pinMode(11,INPUT);
-  pinMode(12,INPUT);
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(chave1,INPUT);
+  pinMode(chave2,INPUT);
 }
 
 void loop() {
-  if (digitalRead(11) == 0 and digitalRead(12) == 0) {
-    digitalWrite(5, 1);
-    digitalWrite(6, 1);
-    digitalWrite(7, 1);
+  if (digitalRead(chave1) == 0 and digitalRead(chave2) == 0) {
+    digitalWrite(led1, 1);
+    digitalWrite(led2, 1);
+    digitalWrite(led3, 1);
   }
 
-  if (digitalRead(11) == 1 and digitalRead(12) == 0) {
-    digitalWrite(5, 1);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
+  if (digitalRead(chave1) == 1 and digitalRead(chave2) == 0) {
+    digitalWrite(led1, 1);
+    digitalWrite(led2, 0);
+    digitalWrite(led3, 0);
   }
 
-  if (digitalRead(11) == 0 and digitalRead(12) == 1) {
-    digitalWrite(5, 0);
-    digitalWrite(6, 1);
-    digitalWrite(7, 0);
+  if (digitalRead(chave1) == 0 and digitalRead(chave2) == 1) {
+    digitalWrite(led1, 0);
+    digitalWrite(led2, 1);
+    digitalWrite(led3, 0);
   }
 
-  if (digitalRead(11) == 1 and digitalRead(12) == 1) {
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
+  if (digitalRead(chave1) == 1 and digitalRead(chave2) == 1) {
+    digitalWrite(led1, 0);
+    digitalWrite(led2, 0);
+    digitalWrite(led3, 0);
   }
-
 }
